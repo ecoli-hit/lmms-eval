cd5ac61ac7f4613cce51d999000197e2f1142b84
HanChenye1122 <1670439315@qq.com>
2024-09-24 14:25:51 +0800

    add MiniCPMv2.6 mplug-owl3

---
+++ lmms_eval/models/mplug_owl3/__init__.py
@@ -0,0 +1,7 @@
+from .configuration_mplugowl3 import *
+from .configuration_hyper_qwen2 import *
+from .image_processing_mplugowl3 import *
+from .modeling_hyper_qwen2 import * 
+from .modeling_mplugowl3 import *
+from .processing_mplugowl3 import *
+from .x_sdpa import *
